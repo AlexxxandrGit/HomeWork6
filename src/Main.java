075e7d7871ef5286222eceeb2caea268a899510a
Alexandr <kitalex777@mail.ru>
2022-10-03 17:02:56 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 public class Main {
 
-    public static void main(String[] args) {
-        for (int i =0; i<=10; i++ ){
+    public static void main(String[] args) {task2();}
+
+    public static void task1() {
+        for (int i =1; i<=10; i++ ){
             System.out.println(i);
         }
 
     }
+
+    public static void task2() {
+
+
+
+    }
+
+
+
+
 }
